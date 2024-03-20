26bcc6022a9a4aa2c20fc85dacef84258a074bd7
MadhusmitaBhola <162480612+MadhusmitaBhola@users.noreply.github.com>
2024-03-20 20:32:27 +0530

    Create script.js
    adding script file

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let HistoryDiv=document.querySelector('.history');
+    let screen=document.querySelector('.screen');
+    let button=document.querySelectorAll('.btn');
+    let history='';
+    button.forEach(function(button){
+        button.addEventListener('click',function(){
+            handleButtonClick(button.innerText);
+        });
+    });
+
+    function handleButtonClick(value){
+        if(value==='C'){
+            clearAll();
+        }else if(value==='DEL'){
+            deleteLastChar();
+        }else if(value==='='){
+            evaluateExpression();
+        }else{
+            appendToScreen(value);
+        }
+    }
+
+    function clearAll(){
+        screen.textContent='';
+        history='';
+        updateHistory();
+    }
+
+    function deleteLastChar(){
+        let currentText=screen.textContent;
+        screen.textContent=currentText.slice(0,-1);
+    }
+
+    function appendToScreen(value){
+        screen.textContent+=value;
+    }
+
+    function evaluateExpression(){
+        try{
+            let expression=screen.textContent;
+            let result=eval(expression);
+            result=parseFloat(result.toFixed(5));
+            history=expression+'='+result;
+            screen.textContent=result;
+            updateHistory();
+        }catch(error){
+            screen.textContent='Error';
+        }
+    }
+
+    function updateHistory(){
+        HistoryDiv.textContent=history;
+    }
+
+
+})
